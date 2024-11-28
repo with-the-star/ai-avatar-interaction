c3bde408164d31015d652f8cf950f561725b3ee5
Magician <jinming.dev@gmail.com>
2024-11-29 04:04:16 +0800

    Update first page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,35 +32,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn('antialiased', inter.className)}>
+      <body>
       <VrmViewer />
         <Providers>
           <div className="flex flex-col">
-            <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between px-4 dark:bg-muted/50">
-              <div className="flex items-center">
-                <Link href="/" rel="nofollow">
-                  <IconNextChat className="mr-2 size-6 dark:hidden" inverted />
-                  <IconNextChat className="mr-2 hidden size-6 dark:block" />
-                </Link>
-                <div className="flex items-center">
-                  <IconSeparator className="size-6 text-muted-foreground/50" />
-                  OpenAI Assistant File Search
-                </div>
-              </div>
-              <div className="flex items-center justify-end space-x-2">
-                <a
-                  target="_blank"
-                  href="https://github.com/talesmousinho/openai-assistant-file-search"
-                  rel="noopener noreferrer"
-                  className={cn('dark:bg-transparent', buttonVariants({ variant: 'outline' }))}
-                >
-                  <IconGitHub />
-                  <span className="ml-2 hidden md:flex">GitHub</span>
-                </a>
-              </div>
-            </header>
-
-            <main className="flex flex-1 flex-col dark:bg-muted/50">{children}</main>
+            <main className="flex flex-1 flex-col">{children}</main>
           </div>
         </Providers>
         <Toaster />
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { useToast } from '@/lib/hooks/use-toast'
 import Dropzone from '@/components/dropzone'
 
 export default function Page() {
+  const [opened, setOpened] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
   const { toast } = useToast()
@@ -48,16 +49,30 @@ export default function Page() {
   }
 
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <div className="p-6 text-gray-700 dark:text-white">
-        {isLoading ? (
-          <LoaderCircle className="size-24 animate-spin" />
-        ) : (
-          <>
-            <p className="mb-4">Get started by uploading your documents here.</p>
-            <Dropzone onLoad={handleFilesLoaded} />
-          </>
-        )}
+    <div className="flex items-center justify-center py-10">
+      <div className="absolute top-10 right-10">
+        <button
+          onClick={() => {
+            setOpened(true)
+          }}
+          className="px-5 py-2 border border-white"
+        >
+          Start
+        </button>
+      </div>
+      <div className="text-gray-700 text-white bg-black bg-opacity-95 rounded-2xl">
+        {opened ? (
+          isLoading ? (
+            <LoaderCircle className="size-24 animate-spin" />
+          ) : (
+            <div className="p-10 space-y-5">
+              <h3 className='text-2xl font-semibold'>Welcome</h3>
+              <p className="">Get started by uploading your documents here.</p>
+              <Dropzone onLoad={handleFilesLoaded} />
+              <p>File Type : PDF, XLSX, TXT, WORD</p>
+            </div>
+          )
+        ) : null}
       </div>
     </div>
   )
